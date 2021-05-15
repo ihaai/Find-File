201d02ff07167144f4c3b9a26b585f52a65a93f3
ihaai <69941441+ihaai@users.noreply.github.com>
2021-05-15 08:44:46 -0700

    Add files via upload

---
+++ FindFile/FindFile/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace FindFile
+{
+    class Program
+    {
+        public static void findIt(string dest, string extension)
+        {
+            string path = $"{dest}";
+            string[] fileNames = Directory.GetFiles(path, $"*.{extension}");
+
+            if (fileNames.Length != 0)
+            {
+                try
+                {
+                    foreach (string fileName in fileNames)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"\tFOUND FILE: '{fileName}' with EXTENSION '{extension}'\n");
+                    }
+                }
+                catch
+                {
+                    Console.Write("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found");
+            }
+            Console.WriteLine($"Total files found: {fileNames.Length}");
+            Console.ReadLine();
+        }
+
+        static void Main()
+        {
+            string one, two;
+            Console.Write("Destination: ");
+            one = Console.ReadLine();
+            Console.Write("Extension (no . needed): ");
+            two = Console.ReadLine();
+            findIt(one, two);
+        }
+    }
+}
